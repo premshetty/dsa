6870b72e1ba77be0110199d8edd49912b82fdb44
PREM KUMAR <premenaga@gmail.com>
2022-12-23 11:37:41 +0530

    read me update

---
+++ README.md
@@ -0,0 +1,6 @@
+# SOLVED DSA PROBLEMS ARE STORED HERE
+
+> # sources
+
+[x] leetcode
+[x] pdf
---
+++ leetcode/medium/int to roman.js
@@ -0,0 +1,19 @@
+const intToRoman = () => {
+    const dict = {
+        "I": 1, //can be in front of "V" or "X"
+        "V": 5,
+        "X": 10, //can be in front of "L" or "C"
+        "L": 50,
+        "C": 100, //can be in front of "D" or "M"
+        "D": 500,
+        "M": 1000
+    }
+    const combintions = {
+        'IV': 4,
+        'IX': 9,
+        'XL': 40,
+        'XC': 90,
+        'CD': 400,
+        'CM': 900
+    }
+}
